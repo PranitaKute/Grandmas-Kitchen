443e4f1e35baf4f9ae3a4d22422499a458f7b91a
Pranita Kute <97874560+PranitaKute@users.noreply.github.com>
2025-03-10 14:28:22 +0530

    Add files via upload

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='home-container'>
+        <div className="home-text">
+            <h1>Grandma's kitchen - where love is the main course.</h1>
+            <p>At Grandma’s Kitchen, every dish tells a story, seasoned with 
+                tradition and served with love. It's more than just food—it's 
+                the warmth of home, the comfort of cherished memories, and the 
+                joy of sharing a meal with family. </p>
+            <button className='btn'>Explore More...</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Home
