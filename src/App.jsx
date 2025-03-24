033813bc49dad3224a5a54fcde232df0b3e4f874
Pranita Kute <97874560+PranitaKute@users.noreply.github.com>
2025-03-24 09:26:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './index.css'
 import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
+import About from './Components/About/About'
+import Recipes from './Components/Recipes/Recipes'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Home/>
+      <About/>
+      {/* <h2>Recipes</h2> */}
+      <Recipes/>
     </div>
   )
 }
